e62b8197bede95e0b2e2e9a490867827d8f0ffda
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-01-22 18:01:57 -0200

    Started testing pods

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -15,7 +15,7 @@ class Tests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        XCTAssertNotNil(Date.today())
     }
     
     func testPerformanceExample() {
