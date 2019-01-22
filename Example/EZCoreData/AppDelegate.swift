94dc41b77283823887aba64698e28932284e038c
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-01-22 17:08:00 -0200

    Started mocking AppDelegate for test cases

--- Example/EZCoreData/AppDelegate.swift
+++ Example/EZCoreData/AppDelegate.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Initialize Core Data:
+//        EZCoreData.databaseName = Constants.databaseName
+//        _ = EZCoreData.shared
+        
         return true
     }
 
---
+++ Example/EZCoreData/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  States.swift
+//  CKL iOS Challenge
+//
+//  Created by Marcelo Salloum dos Santos on 08/01/19.
+//  Copyright © 2019 Marcelo Salloum dos Santos. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let databaseName: String = "CKL_iOS_Challenge"
+    static let idKey: String = "id"
+}
