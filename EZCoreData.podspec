53743badc75c0e6ecf8a4598e5480f828dd0a446
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-01-22 18:00:38 -0200

    Started using Pod from example project

--- EZCoreData.podspec
+++ EZCoreData.podspec
@@ -30,6 +30,7 @@ Pod::Spec.new do |s|
   s.ios.deployment_target = '10.0'
 
   s.source_files = 'EZCoreData/Classes/**/*'
+  s.swift_version = '4.2'
   
   # s.resource_bundles = {
   #   'EZCoreData' => ['EZCoreData/Assets/*.png']
---
+++ EZCoreData/Classes/EZCoreData.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+
+extension Date {
+    public static func today() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormatter.string(from: Date())
+    }
+}
