7793d3634811ab0b58894cd799f2baa1ec9024e9
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-02-19 11:30:27 -0300

    Incorporated PromiseKit into Delete methods

--- EZCoreData/Classes/NSManagedObject+Delete.swift
+++ EZCoreData/Classes/NSManagedObject+Delete.swift
@@ -8,114 +8,56 @@
 
 import Foundation
 import CoreData
-
-// MARK: Basic Functions
-extension NSFetchRequestResult where Self: NSManagedObject {
-    static public func deleteObjects(fromList objectList: [Self],
-                                     _ context: NSManagedObjectContext = EZCoreData.mainThreadContext) throws {
-        let objectCount = objectList.count
-        var objectType: String = "Unknown"
-        for object in objectList {
-            objectType = String(describing: type(of: object))
-            try object.delete(shouldSave: false, context: context)
-        }
-        EZCoreDataLogger.log("Attempting to delete a list of \(objectCount) objects of type '\(objectType)'")
-    }
-
-    static public func deleteObjects(fromList objectList: [Self],
-                                     _ backgroundContext: NSManagedObjectContext = EZCoreData.privateThreadContext,
-                                     completion: @escaping (EZCoreDataResult<[Self]>) -> Void) {
-        backgroundContext.perform {
-            do {
-                try deleteObjects(fromList: objectList, backgroundContext)
-
-                // Save and call return function
-                backgroundContext.saveContextToStore({ (result) in
-                    switch result {
-                    case .success(result: _):
-                        completion(EZCoreDataResult<[Self]>.success(result: nil))
-                    case .failure(error: let error):
-                        completion(EZCoreDataResult<[Self]>.failure(error: error))
-                    }
-                })
-            } catch let error {
-                EZCoreDataLogger.log(error.localizedDescription, verboseLevel: .error)
-                completion(.failure(error: error))
-            }
-        }
-    }
-}
+import PromiseKit
 
 // MARK: - Delete One
 extension NSFetchRequestResult where Self: NSManagedObject {
     /// Delete given object within the given context
-    static public func delete(_ object: Self,
-                              shouldSave: Bool = true,
-                              context: NSManagedObjectContext = EZCoreData.mainThreadContext) throws {
+    static public func delete(_ object: Self, context: NSManagedObjectContext) throws {
         context.delete(object)
-        if shouldSave {
-            try context.save()
-        }
     }
 
     /// Delete the object within the given context
-    public func delete(shouldSave: Bool = true, context: NSManagedObjectContext = EZCoreData.mainThreadContext) throws {
-        try Self.delete(self, shouldSave: shouldSave, context: context)
+    public func delete() {
+        self.managedObjectContext?.delete(self)
     }
 }
 
-// MARK: - Delete All
+// MARK: Delete List
 extension NSFetchRequestResult where Self: NSManagedObject {
+    static public func deleteAll(fromList objectList: [Self],
+                                 _ backgroundContext: NSManagedObjectContext) -> Promise<Void> {
 
-    /// SYNC Delete all objects of this kind except the given list
-    static public func deleteAll(except toKeep: [Self]? = nil,
-                                 context: NSManagedObjectContext = EZCoreData.mainThreadContext) throws {
+        return Promise<Void> { resolver in
+            backgroundContext.perform {
+                let objectCount = objectList.count
+                var objectType: String = "Unknown"
+                EZCoreDataLogger.log("Attempting to delete a list of \(objectCount) objects of type '\(objectType)'")
+                for object in objectList {
+                    objectType = String(describing: type(of: object))
+                    object.delete()
+                }
+                resolver.fulfill_()
+            }
+        }
+    }
+
+    /// Delete all objects returned in the given NSFetchRequest
+    static public func deleteAll(fromPredicate predicate: NSPredicate?,
+                                 backgroundContext: NSManagedObjectContext) -> Promise<Void> {
+        return self.readAll(predicate, context: backgroundContext).then { objectList -> Promise<Void> in
+            self.deleteAll(fromList: objectList, backgroundContext)
+        }
+    }
+
+    /// Delete all objects of this kind except the given list
+    static public func deleteAll(exceptFromList toKeep: [Self]? = nil,
+                                 backgroundContext: NSManagedObjectContext) -> Promise<Void> {
         // Predicate
         var predicate: NSPredicate?
         if let toKeep = toKeep, toKeep.count > 0 {
             predicate = NSPredicate(format: "NOT (self IN %@)", toKeep)
         }
-        // Delete Request
-        try deleteAllFromFetchRequest(predicate, context: context)
-        context.saveContextToStore()
-    }
-
-    /// ASYNC Delete all objects of this kind except the given list
-    static public func deleteAll(except toKeep: [Self]? = nil,
-                                 backgroundContext: NSManagedObjectContext = EZCoreData.privateThreadContext,
-                                 completion: @escaping (EZCoreDataResult<[Self]>) -> Void) {
-        backgroundContext.perform {
-            // Predicate
-            var predicate: NSPredicate?
-            if let toKeep = toKeep, toKeep.count > 0 {
-                predicate = NSPredicate(format: "NOT (self IN %@)", toKeep)
-            }
-            do {
-                // Delete Request
-                try deleteAllFromFetchRequest(predicate, context: backgroundContext)
-                backgroundContext.saveContextToStore({ (result) in
-                    switch result {
-                    case .success(result: _):
-                        completion(EZCoreDataResult<[Self]>.success(result: nil))
-                    case .failure(error: let error):
-                        completion(EZCoreDataResult<[Self]>.failure(error: error))
-                    }
-                })
-            } catch let error {
-                EZCoreDataLogger.log(error.localizedDescription, verboseLevel: .error)
-                completion(.failure(error: error))
-            }
-        }
-    }
-}
-
-// MARK: - Private Funcs
-extension NSFetchRequestResult where Self: NSManagedObject {
-
-    /// Delete all objects returned in the given NSFetchRequest
-    fileprivate static func deleteAllFromFetchRequest(_ predicate: NSPredicate?,
-                                                      context: NSManagedObjectContext) throws {
-        let objectList = try self.readAll(predicate: predicate, context: context)
-        try deleteObjects(fromList: objectList, context)
+        return deleteAll(fromPredicate: predicate, backgroundContext: backgroundContext)
     }
 }
